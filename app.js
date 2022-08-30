c2a05512ba334391db83018c9c363224e57c8205
urvish sojitra <sojitraurvish0@gmail.com>
2022-08-30 18:54:52 +0530

    initial commit

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use(errorHandler);
 
 const PORT=process.env.PORT || 5000;
 
-const server=app.listen(PORT,"localhost",()=>{
+const server=app.listen(PORT,()=>{
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}!...`.yellow.bold);
 });
 
